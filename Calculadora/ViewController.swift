d7e3c37a4d1978267eef4d8e70708f1e642351a5
Christian Manzaraz <magnusmanz@gmial.com>
2022-11-22 22:14:23 -0300

    Initial Commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Christian Manzaraz on 22/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
